ff59448c1116f63065ef7d5db85719e176bf541a
polya <polina.s.nekrasova@gmail.com>
2021-04-20 19:05:15 +0300

    added $ + variable case

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+#include <unistd.h>
 
 void	ft_echo(const char *str)
 {
+	char *dollar;
+	char *newstr;
+	int len;
+	int i = 0;
 	//ft_putstr_fd(stdout, str);
-	printf("%s ", str);
+	if ((dollar = ft_strchr(str, '$')))
+	{
+		while (str[i] != '$')
+		{
+			i++;
+		}
+		len = ft_strlen(dollar);
+		newstr = ft_substr(str, i, len); // possible leak
+		printf("%s\n", getenv(newstr)); //we get the env variable home and change dir to there
+	}
+	else
+	{
+		printf("%s ", str);
+	}
 	//return (0);
 }
