300f0f14c135b144f1875d84daf7e5bc53c75ea5
polya <polina.s.nekrasova@gmail.com>
2021-05-01 16:55:17 +0300

    export trying to fix

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -49,7 +49,7 @@ void 	ft_export(int argc, char **str, char **envp)
 	while (*g_all.env != 0 && i < argc)
 	{
 		this_env = *g_all.env;
-		equal = ft_strchr(this_env, '=');
+		equal = ft_strchr(str[i], '=');
 		if (equal == NULL)
 		{
 			g_all.env = ft_allocate_env_builtins(g_all.env, 1, str[i], this_env);
@@ -57,20 +57,21 @@ void 	ft_export(int argc, char **str, char **envp)
 		else if (equal != NULL)
 		{
 			int j = 1;
-			while (*beg_env != 0 && j < argc)
+			int k = 0;
+			while (g_all.env[k] != NULL && j < argc)
 			{
-				this_env = *beg_env;
-				if (!(ft_strncmp(ft_substr(this_env, 0, ft_strlen(this_env) - ft_strlen(ft_strchr(this_env, '='))),
+				this_env = g_all.env[k];
+				if (!(ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
 								ft_substr(str[j], 0, ft_strlen(str[j]) - ft_strlen(ft_strchr(str[j], '='))),
-								ft_strlen(ft_substr(this_env, 0, ft_strlen(this_env) - ft_strlen(ft_strchr(this_env, '=')))))))
+								ft_strlen(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '=')))))))
 				{
-					printf("%s\n", ft_substr(this_env, 0, ft_strlen(this_env) - ft_strlen(ft_strchr(this_env, '='))));
-					*g_all.env = ft_strdup(str[j]);
-					printf("%s\n", *g_all.env);
-					flag_rep = 1;
+					printf("%s\n", ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(this_env, '='))));
+					g_all.env[k] = ft_strdup(str[j]);
+					printf("%s\n", g_all.env[k]);
+					// flag_rep = 1;
 				}
 				j++;
-				beg_env++;
+				k++;
 			}
 		}
 		i++;
@@ -83,27 +84,28 @@ void 	ft_export(int argc, char **str, char **envp)
 	// 	// g_all.env[i] != isalphanum except for '=' and mb other bash reserved symbols
 	// 	exit (-1);
 	// 	// NEED TO ADD VALUES CHANGE HANDLING
-	// }
+// }
+	int k = 0;
 	// g_all.env = beg_env;
-	// while (*g_all.env != NULL)
-	// {
-	// 	this_env = *g_all.env;
-	// 	equal = ft_strchr(this_env, '=');
-	// 	if (equal == NULL)
-	// 	{
-	// 		printf("%s%s%c", "declare -x ", this_env, '\n');
-	// 	}
-	// 	else
-	// 	{
-	// 		printf("%s", "declare -x ");
-	// 		printf("%s", ft_substr(this_env, 0, ft_strlen(this_env) - ft_strlen(equal)));
-	// 		printf("%c", equal[0]);
-	// 		printf("%c", '"');
-	// 		printf("%s", ft_substr(equal, 1, ft_strlen(equal) - 1));
-	// 		printf("%c%c", '"', '\n');
-	// 	}
-	// 	g_all.env++;
-	// }
+	while (g_all.env[k] != NULL)
+	{
+		this_env = g_all.env[k];
+		equal = ft_strchr(this_env, '=');
+		if (equal == NULL)
+		{
+			printf("%s%s%c", "declare -x ", this_env, '\n');
+		}
+		else
+		{
+			printf("%s", "declare -x ");
+			printf("%s", ft_substr(this_env, 0, ft_strlen(this_env) - ft_strlen(equal)));
+			printf("%c", equal[0]);
+			printf("%c", '"');
+			printf("%s", ft_substr(equal, 1, ft_strlen(equal) - 1));
+			printf("%c%c", '"', '\n');
+		}
+		k++;
+	}
 }
 
 int main (int argc, char **argv, char **envp)
