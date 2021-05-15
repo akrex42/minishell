afc0f4fbe64281c2d53c2196cc2c658da37c643e
polya <polina.s.nekrasova@gmail.com>
2021-05-16 00:15:28 +0300

    I definitely need to rewrite export

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -4,50 +4,21 @@ void 	ft_export(int argc, char **str, char **envp)
 {
 	char *equal;
 	char *this_env;
-	// char *key1;
-	// char *key2;
 	int i;
 	int j;
 	int k;
-	int l;
-	char **beg_env;
 	int flag_rep;
 	int flag_equal = 0;
 
 	flag_rep = 0;
 
 	g_all.env = ft_allocate_env(envp); // we need to initialize it somewhere else
-	i = 1;
-	beg_env = g_all.env;
+	i = 1; // Program name may be omitted with the use of exec* family
 
 	while (i < argc)
 	{
 		equal = ft_strchr(str[i], '=');
-		if (equal != NULL && flag_rep == 0)
-		{
-			j = 1;
-			k = 0; 
-			while ((g_all.env[k] != NULL) && (j < argc))
-			{
-				this_env = g_all.env[k];
-				equal = ft_strchr(str[j], '=');
-				if (equal == NULL)
-				{
-					j++;
-				}
-				else if (!(ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
-					ft_substr(str[j], 0, ft_strlen(str[j]) - ft_strlen(ft_strchr(str[j], '='))),
-					ft_strlen(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '=')))))))
-				{
-					g_all.env[k] = ft_strdup(str[j]);
-					flag_rep = 1;
-					j++;
-					k = 0;
-				}
-				k++;
-			}
-		}
-		else if (equal == NULL)
+		if (equal == NULL)
 		{
 			k = 0;
 			while (g_all.env[k] != NULL) // if key has no value??
@@ -58,37 +29,18 @@ void 	ft_export(int argc, char **str, char **envp)
 					if (!(ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
 							str[i], ft_strlen(str[i]))))
 					{
-						// printf("%s\n", str[i]);
+						// printf("%s\n", str[i]); // if keys without values repeat
 						flag_equal = 1;
 						// break ;
 					}
 				}
 				k++;
 			}
-			if (flag_equal == 0) // h h case breaks
+			if (flag_equal == 0)
 			{
-				k = 0;
-				while (g_all.env[k] != NULL)
-				{
-					equal = ft_strchr(g_all.env[k], '=');
-					if (equal != NULL)
-					{
-						if (ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
-								str[i], ft_strlen(str[i])) == 0) //we need to compare the lenght of the arg, not the existing env
-						{
-							printf("%s\n", str[i]);
-							flag_equal = 2;
-							break ;
-						}
-					}
-					k++;
-				}
-				if (flag_equal == 0)
-				{
-					// printf("%s\n", str[i]);
-					g_all.env = ft_allocate_env_builtins(g_all.env, 1, str[i], this_env);
-				}		
-			}
+				// printf("%s\n", str[i]); // all the keys without value that do not repeat
+				g_all.env = ft_allocate_env_builtins(g_all.env, 1, str[i], this_env);
+			}		
 			flag_equal = 0;
 		}
 		else if (equal != NULL) // I need to add existing key WITHOUT value case
@@ -98,6 +50,8 @@ void 	ft_export(int argc, char **str, char **envp)
 			{
 				if (!(ft_strncmp(g_all.env[k], str[i], ft_strlen(g_all.env[k]))))
 				{
+					printf("%s\n", str[i]); // all the keys with value FIRST VALUE
+					// g_all.env = ft_allocate_env_builtins(g_all.env, 1, str[i], this_env);
 					flag_equal = 1;
 					break ;
 				}
@@ -111,19 +65,41 @@ void 	ft_export(int argc, char **str, char **envp)
 					equal = ft_strchr(g_all.env[k], '=');
 					if (equal != NULL)
 					{
-						if (ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
-								ft_substr(str[i], 0, ft_strlen(str[i]) - ft_strlen(ft_strchr(str[i], '='))),
-								ft_strlen(ft_substr(str[i], 0, ft_strlen(str[i]) - ft_strlen(ft_strchr(str[i], '='))))) == 0)
+						if (ft_strlen(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '=')) 
+							> ft_strlen(ft_substr(str[i], 0, ft_strlen(str[i]) - ft_strlen(ft_strchr(str[i], '=')))))))
+							{
+								if (ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
+										ft_substr(str[i], 0, ft_strlen(str[i]) - ft_strlen(ft_strchr(str[i], '='))),
+										ft_strlen(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))))) == 0)
+								{
+									// printf("%s\n", str[i]); // final versions of key=value
+									// printf("%s\n", g_all.env[k]);
+									g_all.env[k] = ft_strdup(str[i]);
+									// printf("%s\n", g_all.env[k]);
+									flag_equal = 1;
+									break ;
+								}
+							}
+						else
 						{
-							flag_equal = 2;
-							break ;
+								if (ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
+										ft_substr(str[i], 0, ft_strlen(str[i]) - ft_strlen(ft_strchr(str[i], '='))),
+										ft_strlen(ft_substr(str[i], 0, ft_strlen(str[i]) - ft_strlen(ft_strchr(str[i], '='))))) == 0)
+								{
+									// printf("%s\n", str[i]); // final versions of key=value
+									// printf("%s\n", g_all.env[k]);
+									g_all.env[k] = ft_strdup(str[i]);
+									// printf("%s\n", g_all.env[k]);
+									flag_equal = 1;
+									break ;
+								}
 						}
 					}
 					k++;
 				}
 				if (flag_equal == 0)
 				{
-					// printf("%s\n", str[i]);
+				// printf("%s\n", str[i]); // first and final versions of key=value
 					g_all.env = ft_allocate_env_builtins(g_all.env, 1, str[i], this_env);
 				}
 			}
@@ -140,25 +116,9 @@ void 	ft_export(int argc, char **str, char **envp)
 						ft_substr(str[j], 0, ft_strlen(str[j]) - ft_strlen(ft_strchr(str[j], '='))),
 						ft_strlen(g_all.env[k]))))
 					{
+						// printf("%s\n", str[i]);
 						g_all.env[k] = ft_strdup(str[j]);
-						break ;
-					}
-				}
-				k++;
-			}
-			j = i;
-			k = 0; 
-			while (g_all.env[k] != NULL)
-			{
-				equal = ft_strchr(str[j], '=');
-				char *equal1 = ft_strchr(g_all.env[k], '=');
-				if (equal1 != NULL)
-				{
-					if (!(ft_strncmp(ft_substr(g_all.env[k], 0, ft_strlen(g_all.env[k]) - ft_strlen(ft_strchr(g_all.env[k], '='))),
-									ft_substr(str[j], 0, ft_strlen(str[j]) - ft_strlen(ft_strchr(str[j], '='))),
-									ft_strlen(ft_substr(str[j], 0, ft_strlen(str[j]) - ft_strlen(ft_strchr(str[j], '=')))))))
-					{
-						g_all.env[k] = ft_strdup(str[j]);
+						// printf("%s\n", g_all.env[k]);
 						break ;
 					}
 				}
