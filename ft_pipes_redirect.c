6fe63c016c8be6de3483cf3aff3d1e2d569ec792
polya <polina.s.nekrasova@gmail.com>
2021-05-21 21:34:59 +0300

    started pipes

--- ft_pipes_redirect.c
+++ ft_pipes_redirect.c
@@ -4,11 +4,12 @@ void	ft_execute()
 {
 	int tmpin = dup(0);
 	int tmpout = dup(1);
+	int fdpipe[20];
 
 	int fdin;
-	if (infile) // input file g.txt < infile
+	if (!ft_strncmp(g_all.commands->special, '<', 2)) // input file g.txt < infile
 	{
-		fdin = open(infile, O_RDONLY);
+		fdin = open(infile, O_RDONLY, O_CREAT); // g_all.commans->args[after the <]
 	}
 	else
 	{
@@ -19,15 +20,15 @@ void	ft_execute()
 	int fdout;
 	int i = 0;
 
-	while (i < numofcommands)
+	while (g_all.commands->next)
 	{
 		dup2(fdin, 0);
 		close(fdin);
-		if (i == numofcommands - 1)
+		if (g_all.commands->next != NULL) // ??
 		{
-			if (outfile) // output file g.txt > outputfile
+			if (!ft_strncmp(g_all.commands->special, '>', 2)) // output file g.txt > outputfile
 			{
-				fdout = open(outfile, O_RDWR);
+				fdout = open(outfile, O_RDWR, O_CREAT); // if we use builtins then we need to change stdout for ft_putstr and ft_putchar, so give one to the current file
 			}
 			else
 			{
@@ -36,7 +37,6 @@ void	ft_execute()
 		}
 		else
 		{
-			int fdpipe[20];
 			pipe(fdpipe);
 			fdout = fdpipe[1];
 			fdin = fdpipe[0];
@@ -47,17 +47,29 @@ void	ft_execute()
 		ret = fork();
 		if (ret == 0)
 		{
-			execvp(scmd[i].args[0], scmd[i].args); //array of commands parsed
-			perror("execvp");
+			if (execve(g_all.commands->prog, g_all.commands->args, NULL)) //array of commands parsed
+			{
+				strerror(errno);
+				exit (1);
+			}
+		}
+		if (ret == -1)
+		{
+			strerror(errno);
 			exit (1);
 		}
+		g_all.commands = g_all.commands->next;
 	}
 	dup2(tmpin, 0);
 	dup2(tmpout, 1);
 	close(tmpin);
 	close(tmpout);
-	if (!background)
-	{
-		waitpid(ret, NULL);
-	}
+	// if (ret != 0)
+	// {
+	// 	// close(fdpipe[0]);          /* Close unused read end */
+    //     // write(fdpipe[1], argv[1], strlen(argv[1]));
+    //     // close(fdpipe[1]);          /* Reader will see EOF */
+    //     // wait(NULL);                /* Wait for child */
+    //     // exit(EXIT_SUCCESS);
+	// }
 }
