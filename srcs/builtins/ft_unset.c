8a7ccb58b9d17e31f5c5b7dce466da7f6b1baceb
polya <polina.s.nekrasova@gmail.com>
2021-06-09 16:27:14 +0300

    builtins ready

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -75,7 +75,7 @@ int	print_err_unset(char *str)
 		ft_putstr_fd(str, 2);
 		ft_putstr_fd("': not a valid identifier", 2);
 		ft_putchar_fd('\n', 2);
-		return (1); // errno?
+		return (1);
 	}
 	while (str[i] != '\0')
 	{
@@ -94,30 +94,14 @@ int	print_err_unset(char *str)
 
 int	ft_unset(char **str)
 {
-	char	*key;
 	int		i;
-	int		j;
-	char	**env2;
 	char	*equal;
 	int		flag_return;
 
 	g_all.flag_allocate = 0;
 	flag_return = 0;
 	i = 1;
-	while (str[i] != NULL)
-	{
-		flag_return = print_err_unset(str[i]);
-		if (flag_return)
-		{
-			i++;
-			continue ;
-		}
-		env2 = g_all.env;
-		j = 0;
-		while (env2[j] != NULL)
-			j = inner_circle3(j, i, str, equal);
-		i++;
-	}
+	flag_return = ft_cycle_unset(str, i, equal);
 	g_all.flag_allocate = 0;
 	if (flag_return)
 		return (1);
--- srcs/builtins/print_err_export.c
+++ srcs/builtins/print_err_export.c
@@ -8,95 +8,78 @@ static int	ft_isspace(char c)
 	return (0);
 }
 
-int	print_err_export(char *str)
+int	ft_print_err_not_equal(char *str)
+{
+	char	*key;
+	int		i;
+
+	i = 0;
+	key = ft_substr(str, 0, ft_strlen(str)
+			- ft_strlen(ft_strchr(str, '=')));
+	while (key[i] != '\0')
+	{
+		if (!ft_isalnum(key[i])
+			&& (key[i] != '_') && (key[i] != '\\')
+			&& (key[i] != '\'')
+			&& (!ft_isspace(key[i])))
+		{
+			ft_putstr_fd("my_bash: export: `", 2);
+			ft_putstr_fd(str, 2);
+			ft_putstr_fd("': not a valid identifier", 2);
+			ft_putchar_fd('\n', 2);
+			return (1);
+		}
+		i++;
+	}
+	free(key);
+	return (0);
+}
+
+int	ft_print_err_equal(char *str)
 {
 	int	i;
-	char *key;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!ft_isalnum(str[i])
+			&& (str[i] != '_') && (str[i] != '\\')
+			&& (str[i] != '\''))
+		{
+			ft_putstr_fd("my_bash: export: `", 2);
+			ft_putstr_fd(str, 2);
+			ft_putstr_fd("': not a valid identifier", 2);
+			ft_putchar_fd('\n', 2);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	print_err_export(char *str)
+{
+	int		i;
+	char	*key;
 
 	i = 1;
 	if (!ft_isalpha(str[0]) && (str[0] != '_'))
 	{
-		ft_putstr_fd("my_bash: export: `", 2);
-		ft_putstr_fd(str, 2);
-		ft_putstr_fd("': not a valid identifier", 2);
-		ft_putchar_fd('\n', 2);
-		return (1); // errno?
+		if (print(str))
+			return (1);
 	}
 	if (str[0] == '\0')
 	{
-		ft_putstr_fd("my_bash: export: `", 2);
-		ft_putchar_fd(str[0], 2);
-		ft_putstr_fd("': not a valid identifier", 2);
-		ft_putchar_fd('\n', 2);
-		return (1);
+		if (print_1(str[0]))
+			return (1);
 	}
 	if (!ft_strchr(str, '='))
 	{
-		i = 0;
-		while (str[i] != '\0')
-		{
-			if (!ft_isalnum(str[i])
-			&& (str[i] != '_') && (str[i] != '\\') 
-			&& (str[i] != '\''))
-			{
-				ft_putstr_fd("my_bash: export: `", 2);
-				ft_putstr_fd(str, 2);
-				ft_putstr_fd("': not a valid identifier", 2);
-				ft_putchar_fd('\n', 2);
-				return (1);
-			}
-			i++;
-		}
+		if (ft_print_err_equal(str))
+			return (1);
 	}
 	else
-	{
-		i = 0;
-		key = ft_substr(str, 0, ft_strlen(str)
-			- ft_strlen(ft_strchr(str, '=')));
-		while (key[i] != '\0')
-		{
-			if (!ft_isalnum(key[i])
-			&& (key[i] != '_') && (key[i] != '\\') 
-			&& (key[i] != '\'')
-			&& (!ft_isspace(key[i])))
-			{
-				ft_putstr_fd("my_bash: export: `", 2);
-				ft_putstr_fd(str, 2);
-				ft_putstr_fd("': not a valid identifier", 2);
-				ft_putchar_fd('\n', 2);
-				return (1);
-			}
-			i++;
-		}
-		free(key);	
-	}
-
-	// while (str[i] != '\0')
-	// {
-	// 	if (str[i] != '\"')
-	// 	{
-	// 		if (!ft_isalnum(str[i]) && (str[i] != '=') 
-	// 		&& (str[i] != '_')
-	// 		&& (str[i] != '\\') && (str[i] != '\'')
-	// 		&& (!ft_isspace(str[i])))
-	// 		{
-	// 			ft_putstr_fd("bash: export: `", 2);
-	// 			ft_putstr_fd(str, 2);
-	// 			ft_putstr_fd("': not a valid identifier", 2);
-	// 			ft_putchar_fd('\n', 2);
-	// 			return (1);
-	// 		}
-	// 	}
-	// 	else if (str[i] == '\"')
-	// 	{
-	// 		i++;
-	// 		while ((str[i] != '\"') && (str[i] != '\0'))
-	// 		{
-	// 			i++;
-	// 		}
-	// 		//  ft_putstr_fd("here", 2);
-	// 	}
-	// 	i++;
-	// }
+		if (ft_print_err_not_equal(str))
+			return (1);
 	return (0);
 }
